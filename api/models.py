82ff360b46d032686bf445c023dc06ee877aea40
Shrinivas Choudhary <shrinivaschowdhary97@gmail.com>
2020-07-06 16:35:01 +0530

    Added comments

--- api/models.py
+++ api/models.py
@@ -3,15 +3,18 @@ from django.db import models
 # Create your models here.
 
 
+# Model for storing activity periods of the members
 class ActivityPeriod(models.Model):
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
 
 
+# Model to display the members
 class MembersList(models.Model):
     ok = models.BooleanField(default=False)
 
 
+# Model representing the member
 class User(models.Model):
     id = models.CharField(max_length=9, primary_key=True)
     real_name = models.CharField(max_length=100)
--- api/views.py
+++ api/views.py
@@ -6,6 +6,7 @@ from .serializers import MemberListSerializer
 # Create your views here.
 
 
+# Send the members list data
 class MembersList(generics.ListAPIView):
     queryset = MembersList.objects.all()
     serializer_class = MemberListSerializer
